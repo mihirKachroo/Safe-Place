ebb0c2d8a5063864bac13ca06c18450dd58a6dc0
Mihir-Kachroo <mkachroo29@gmail.com>
2020-04-12 10:39:31 -0400

    finished

--- Safe Place Web App/public/assets/mail/locate_me.php
+++ Safe Place Web App/public/assets/mail/locate_me.php
@@ -7,9 +7,25 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 
-// Create the email and send the message
-$to = "yourname@yourdomain.com";
+// We define our address
+$address = strip_tags(htmlspecialchars($_POST['name']));
+echo"<PRE>";
+print_r(get_lat_long($address));
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
-?>
+// function to get  the address
+function get_lat_long($address) {
+   $array = array();
+   $geo = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($address).'&sensor=false');
+
+   // We convert the JSON to an array
+   $geo = json_decode($geo, true);
+
+   // If everything is cool
+   if ($geo['status'] = 'OK') {
+      $latitude = $geo['results'][0]['geometry']['location']['lat'];
+      $longitude = $geo['results'][0]['geometry']['location']['lng'];
+      $array = array('lat'=> $latitude ,'lng'=>$longitude);
+   }
+
+   return $array;
+}
