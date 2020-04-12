6d1eb1b8c243292f9c6ed58d858f8e9c29d16155
Mihir-Kachroo <mkachroo29@gmail.com>
2020-04-12 06:47:29 -0400

    before firebase

--- Safe Place Web App/src/userlocation.js
+++ Safe Place Web App/src/userlocation.js
@@ -1,2 +1,9 @@
-export default [34.0522, -118.2437];
+var globalVariable = {
+    lat: '34.0522',
+    long: '-118.2437'
+};
+var pointlat = parseFloat(globalVariable.lat);
+var pointlong = parseFloat(globalVariable.long);
+
+export default [pointlat, -118.2437];
 //OG is [34.0522, -118.2437]
